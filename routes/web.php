52449679b47ed96005df2a14fec8db5351f4a380
semoog <sebmernst@gmail.com>
2017-02-09 16:37:56 -0700

    Basic

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +12,40 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  $tasks = App\Task::orderBy('created_at', 'asc')->get();
+
+  return view('tasks', [
+     'tasks' => $tasks
+  ]);
 });
+
+/**
+ * Add A New Task
+ */
+ Route::post('/task', function (Request $request) {
+     $validator = Validator::make($request->all(), [
+         'name' => 'required|max:255',
+     ]);
+
+     if ($validator->fails()) {
+         return redirect('/')
+             ->withInput()
+             ->withErrors($validator);
+     }
+
+     // Create The Task...
+     $task = new App\Task;
+     $task->name = $request->name;
+     $task->save();
+
+     return redirect('/');
+ });
+
+/**
+ * Delete An Existing Task
+ */
+ Route::delete('/task/{id}', function ($id) {
+     App\Task::findOrFail($id)->delete();
+
+     return redirect('/');
+ });
